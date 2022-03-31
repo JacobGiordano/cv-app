ddcde593e3474c2d012976efc223fee928fbdc7b
JacobGiordano <jacob.giordano12@gmail.com>
2022-03-31 07:01:23 -0700

    Add new components for Inputs, Preview & Header

---
+++ src/components/Inputs/Inputs.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../Inputs/Inputs.css"
+
+class Inputs extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+  render() {
+    return <div>Inputs</div>
+  }
+}
+
+export default Inputs;
---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import "../Preview/Preview.css";
+
+class Preview extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return <div>Preview</div>;
+  }
+}
+
+export default Preview;
