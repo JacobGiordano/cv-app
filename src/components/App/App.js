121bad3d374b35adb1f4bf11c395bc76c974a925
JacobGiordano <jacob.giordano12@gmail.com>
2022-04-01 06:24:27 -0700

    Add App state values

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,81 @@ import "../../components/App/App.css";
 import Header from "../Header/Header";
 import Inputs from "../Inputs/Inputs";
 import Preview from "../Preview/Preview";
+import uniqid from "uniqid";
 
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state ={
-
+      first_name: "",
+      last_name: "",
+      phone: "",
+      email: "",
+      location: "",
+      city: "",
+      state_province: "",
+      sites: [
+        {
+          id: uniqid(),
+          site: ""
+        }
+      ],
+      profile: "",
+      work_experience: [
+        {
+          id: uniqid(),
+          company: "",
+          role_position: "",
+          location: "",
+          start_date: "",
+          end_date: "",
+          responsibilities: [
+            {
+              id: uniqid(),
+              responsibility: ""
+            }
+          ]
+        }
+      ],
+      project: [
+        {
+          id: uniqid(),
+          project: "",
+          role_position: "",
+          location: "",
+          start_date: "",
+          end_date: "",
+          responsibilities: [
+            {
+              id: uniqid(),
+              responsibility: ""
+            }
+          ]
+        }
+      ],
+      skills: [
+        {
+          id: uniqid(),
+          skill: ""
+        }
+      ],
+      education: [
+        {
+          id: uniqid(),
+          degree_certificate: "",
+          area_of_study: "",
+          school_institution: "",
+          start_date: "",
+          end_date: ""
+        }
+      ],
+      certificates_associations: [
+        {
+          id: uniqid(),
+          certificate_association: ""
+        }
+      ]
     }
   }
   render() {
