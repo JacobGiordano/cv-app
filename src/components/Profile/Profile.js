1abbc16d6241694d4231f9496936a35d55210069
JacobGiordano <jacob.giordano12@gmail.com>
2022-04-13 06:58:32 -0700

    Add onChange for Profile & state updates, remove conditional rendering

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,13 +9,20 @@ class Profile extends Component {
       profile_text: ""
     }
   }
+  handleChange = (e) => {
+    this.setState({profile_text: e.target.value});
+  }
   render() {
     const sectionId = uniqid();
     return (
-      this.state.profile_text && <>
+      <>
         <h3>Profile</h3>
         <label htmlFor={`profile${sectionId}`}>Profile (optional)</label>
-        <textarea id={`profile${sectionId}`}></textarea>
+        <textarea
+          id={`profile${sectionId}`}
+          onChange={e => this.handleChange(e)}
+          value={this.state.profile_text}
+        ></textarea>
       </>
     );
   }
