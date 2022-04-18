5a03ee3f7cc225a5c291b3f4b2fd9c9e1783d473
JacobGiordano <jacob.giordano12@gmail.com>
2022-04-18 06:56:45 -0700

    Add contact website functions & clean up code for readability

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
   constructor(props) {
     super(props)
 
-    this.state ={
+    this.state = {
       first_name: "",
       last_name: "",
       phone: "",
@@ -21,20 +21,37 @@ class Contact extends Component {
       ],
     }
   }
+ 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+  handleAddWebsite = () => {
+    this.setState({websites: [...this.state.websites, {
+      id: uniqid(),
+      site: ""
+    }]});
+  }
+  handleChangeWebsite = (e) => {
+    const indexNum = parseInt(e.target.id.split("_")[1]);
+    const newWebsitesArray = this.state.websites.map((site, i) => {
+      return i === indexNum ? { id: site.id, site: e.target.value } : site
+    });
+    this.setState({websites: newWebsitesArray});
+  }
   render() {
-    const sectionId = uniqid();
     let websitesArray = [];
     this.state.websites.map((website, i) => {
       let labelText = `Website #${i + 1}`;
       return websitesArray.push(
-        <div key={uniqid()}>
+        <div key={i}>
           <label
-            htmlFor={`website_${sectionId}_${i}`}
+            htmlFor={`website_${i}`}
           >{labelText}</label>
           <input
             type="text"
-            id={`website_${sectionId}_${i}`}
-            // value={website.site}
+            id={`website_${i}`}
+            onChange={e => this.handleChangeWebsite(e)}
+            value={website.site}
           />
         </div>
       )
@@ -43,26 +60,61 @@ class Contact extends Component {
       <>
         <h3>Contact</h3>
         <div>
-          <label htmlFor={`first_name_${sectionId}`}>First Name</label>
-          <input type="text" id={`first_name_${sectionId}`} />
-          <label htmlFor={`last_name_${sectionId}`}>Last Name</label>
-          <input type="text" id={`last_name_${sectionId}`} />
+          <label htmlFor={`first_name`}>First Name</label>
+          <input 
+            type="text" id={`first_name`}
+            name="first_name"
+            value={this.state.first_name}
+            onChange={e => this.handleChange(e)}
+          />
+          <label htmlFor={`last_name`}>Last Name</label>
+          <input 
+            type="text" id={`last_name`}
+            name="last_name"
+            value={this.state.last_name}
+            onChange={e => this.handleChange(e)}
+          />
         </div>
         <div>
-          <label htmlFor={`phone_${sectionId}`}>Phone Number</label>
-          <input type="tel" id={`phone_${sectionId}`} />
-          <label htmlFor={`email_${sectionId}`}>Email</label>
-          <input type="email" id={`email_${sectionId}`} />
+          <label htmlFor={`phone`}>Phone Number</label>
+          <input 
+            type="tel" id={`phone`}
+            name="phone"
+            value={this.state.phone}
+            onChange={e => this.handleChange(e)}
+          />
+          <label htmlFor={`email`}>Email</label>
+          <input 
+            type="email" id={`email`}
+            name="email"
+            value={this.state.email}
+            onChange={e => this.handleChange(e)}
+          />
         </div>
         <div>
-          <label htmlFor={`city_${sectionId}`}>City</label>
-          <input type="text" id={`city_${sectionId}`} />
-          <label htmlFor={`state_province${sectionId}`}>State / Province</label>
-          <input type="text" id={`state_province${sectionId}`} />
+          <label htmlFor={`city`}>City</label>
+          <input 
+            type="text" id={`city`}
+            name="city"
+            value={this.state.city}
+            onChange={e => this.handleChange(e)}
+          />
+          <label htmlFor={`state_province`}>State / Province</label>
+          <input 
+            type="text" id={`state_province`}
+            name="state_province"
+            value={this.state.state_province}
+            onChange={e => this.handleChange(e)}
+          />
         </div>
         <div>
           {websitesArray}
         </div>
+        <button
+          onClick={() => this.handleAddWebsite()}
+        >
+          + Add website
+        </button>
       </>
     )
   }
