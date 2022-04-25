3aeb8e9ed5bbe49877a486da9d71e76db5d500f4
JacobGiordano <jacob.giordano12@gmail.com>
2022-04-25 06:56:04 -0700

    Move Input Components to Inputs folder

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import AchievementsAndAssociations from "../AchievementsAndAssociations/AchievementsAndAssociations";
-import Contact from "../Contact/Contact";
-import Education from "../Education/Education";
-import Profile from "../Profile/Profile";
-import Projects from "../Projects/Projects";
-import Skills from "../Skills/Skills";
-import WorkExperience from "../WorkExperience/WorkExperience";
+import AchievementsAndAssociations from "./AchievementsAndAssociations/AchievementsAndAssociations";
+import Contact from "./Contact/Contact";
+import Education from "./Education/Education";
+import Profile from "./Profile/Profile";
+import Projects from "./Projects/Projects";
+import Skills from "./Skills/Skills";
+import WorkExperience from "./WorkExperience/WorkExperience";
 
 class Inputs extends Component {
   constructor(props) {
